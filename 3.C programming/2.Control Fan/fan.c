4539686427657b6c9775aa3f86652a93a7101c77
Yahboom Technology <35594203+YahboomTechnology@users.noreply.github.com>
2021-09-10 11:18:50 +0800

    Add files via upload

--- 3.C programming/2.Control Fan/fan.c
+++ 3.C programming/2.Control Fan/fan.c
@@ -19,55 +19,19 @@ int main(void)
 // Loop to let the state increase by 1, each time you add 1 to send a command to adjust the speed of 
 	while (1)
 	{
-		switch (state)
-		{
-			case 0:
-				wiringPiI2CWriteReg8(fd_i2c, 0x08, 0x00);
-			break;
-			case 1:
-				wiringPiI2CWriteReg8(fd_i2c, 0x08, 0x02);
-			break;
-			case 2:
-				wiringPiI2CWriteReg8(fd_i2c, 0x08, 0x03);
-			break;
-			case 3:
-				wiringPiI2CWriteReg8(fd_i2c, 0x08, 0x04);
-			break;
-			case 4:
-				wiringPiI2CWriteReg8(fd_i2c, 0x08, 0x05);
-			break;
-			case 5:
-				wiringPiI2CWriteReg8(fd_i2c, 0x08, 0x06);
-			break;
-			case 6:
-				wiringPiI2CWriteReg8(fd_i2c, 0x08, 0x07);
-			break;
-			case 7:
-				wiringPiI2CWriteReg8(fd_i2c, 0x08, 0x08);
-			break;
-			case 8:
-				wiringPiI2CWriteReg8(fd_i2c, 0x08, 0x09);
-			break;
-			case 9:
-				wiringPiI2CWriteReg8(fd_i2c, 0x08, 0x01);
-			break;
-			default:
-			break;
-		}
 		if (state == 0)
 		{
-			delay(1000);
+			wiringPiI2CWriteReg8(fd_i2c, 0x08, 0x00);
+			delay(2000);
 		}
-
-		state++;
-
-		if (state > 9)
+		else
 		{
-			delay(1000);
-			state = 0;
+			wiringPiI2CWriteReg8(fd_i2c, 0x08, 0x01);
+			delay(2000);
 		}
+		
+		state = (state + 1) % 2;
 
-		delay(1000);
 	}
 	return 0;
 }
