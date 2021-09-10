9dcca03b23baac1ac13c03dd191e589443dc3668
Yahboom Technology <35594203+YahboomTechnology@users.noreply.github.com>
2021-09-10 11:23:24 +0800

    Add files via upload

--- 4.Python programming/2.Control Fan/fan.py
+++ 4.Python programming/2.Control Fan/fan.py
@@ -1,3 +1,4 @@
+
 import smbus
 import time
 bus = smbus.SMBus(1)
@@ -11,28 +12,8 @@ while True:
         bus.write_byte_data(addr, fan_reg, 0x00)
         time.sleep(2)
     elif state == 1:
-        bus.write_byte_data(addr, fan_reg, 0x02)
-    elif state == 2:
-        bus.write_byte_data(addr, fan_reg, 0x03)
-    elif state == 3:
-        bus.write_byte_data(addr, fan_reg, 0x04)
-    elif state == 4:
-        bus.write_byte_data(addr, fan_reg, 0x05)
-    elif state == 5:
-        bus.write_byte_data(addr, fan_reg, 0x06)
-    elif state == 6:
-        bus.write_byte_data(addr, fan_reg, 0x07)
-    elif state == 7:
-        bus.write_byte_data(addr, fan_reg, 0x08)
-    elif state == 8:
-        bus.write_byte_data(addr, fan_reg, 0x09)
-    elif state == 9:
         bus.write_byte_data(addr, fan_reg, 0x01)
+        time.sleep(2)
 
-    state += 1
-
-    if state > 9:
-        time.sleep(1)
-        state = 0
-    time.sleep(1)
+    state = (state + 1) % 2
     
